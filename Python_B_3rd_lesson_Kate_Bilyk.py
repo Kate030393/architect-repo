0a85b44bf5443f253a87086e6617f9c79a835940
Froom32 <bilikmike@gmail.com>
2024-01-06 00:06:54 +0100

    Changes task 1

--- Python_B_3rd_lesson_Kate_Bilyk.py
+++ Python_B_3rd_lesson_Kate_Bilyk.py
@@ -1,6 +1,8 @@
 # TASK 1
 try:
     user_choice = int(input("Which day of the week you want to see? Enter a number from 1 to 7: "))
+    if user_choice < 1 or user_choice > 7:
+        raise Exception("Error of input. Please enter a number from 1 to 7!")
     match user_choice:
         case 1:
             print("Monday is the 1st day of the week")
@@ -16,10 +18,8 @@ try:
             print("Saturday is the 6th day of the week")
         case 7:
             print("Sunday is the 7th day of the week")
-    if user_choice < 1 or user_choice > 7:
-        raise Exception("Error of input. Please enter a number from 1 to 7!")
 except ValueError as error:
-    print("Error of input. Enter a number from 1 to 7 please!")
+    print("Your input is not an integer. Please enter a number from 1 to 7!")
 except Exception as error:
     print(f"{error}")
 
